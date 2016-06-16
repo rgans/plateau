8b57fdb0455dfe88b8865a769161bf3cc680bf4d
Rafael Rocha Gans <rgans@Wanderson.local>
2016-06-16 15:24:07 -0300

    no message

---
+++ plateau.rolling/src/CalendarViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  plateau.rolling
+//
+//  Created by Rafael Rocha Gans on 6/16/16.
+//  Copyright (c) 2016 heresy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
